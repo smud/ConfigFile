ddc334a3ce72b109efdfb60a597afc49f4aa1ab4
Andrey Fidrya <af@zabiyaka.com>
2016-07-24 08:28:17 +0000

    Link to LinuxCompatibility package

--- Package.swift
+++ Package.swift
@@ -2,4 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "ConfigFile"
+    dependencies: [
+        .Package(url: "https://github.com/smud/LinuxCompatibility.git", majorVersion: 0),
+    ]
 )
